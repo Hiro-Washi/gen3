9fc473c1dde80f27b7943819dfc6748def7e2407
Hiro-Washi <c1115332@planet.kanazawa-it.ac.jp>
2022-09-21 20:41:41 +0900

    first

---
+++ apps_gen3/chaser_wh/src/chaser19.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def DEG2RAD
+
+def RAD2DEG
+
+def floatLimit(value)
+
+class Pose():
+
+class Object():
+    def getX
+    def getY
+    def getX
+    def getTheta
+    def setX
+    def setY
+    def setZ
+    def setTheta
+human = Object()
+
+class Robot():
+    def __init__(self):
+        
